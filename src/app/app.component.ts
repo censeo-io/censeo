04f328072c37fcbebc5332bc3a06c40e9bd96820
Clem Flory <cjflory@gmail.com>
2023-01-28 22:32:51 -0600

    add nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Optional } from '@angular/core';
+import {
+  Auth,
+  authState,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  User,
+} from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { map, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'censeo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  navMenuItems$!: Observable<MenuItem[]>;
+
+  constructor(
+    private readonly router: Router,
+    @Optional() private readonly auth: Auth
+  ) {}
+
+  ngOnInit(): void {
+    this.navMenuItems$ = this.auth
+      ? authState(this.auth).pipe(map((user) => this.getNavItems(user)))
+      : of(this.getNavItems(null));
+  }
+
+  private getNavItems(user: User | null): MenuItem[] {
+    const baseNavItems = [
+      {
+        label: 'Censeo',
+        routerLink: '/',
+        styleClass: 'font-bold',
+        routerLinkActiveOptions: { exact: true },
+      },
+    ];
+
+    if (user) {
+      // User is logged in
+      return [
+        ...baseNavItems,
+        {
+          label: 'Meetings',
+          icon: 'pi pi-fw pi-calendar',
+          routerLink: 'meetings',
+        },
+        {
+          label: 'Users',
+          icon: 'pi pi-fw pi-users',
+          routerLink: 'users',
+        },
+        {
+          label: user.displayName,
+          items: [
+            {
+              label: 'Logout',
+              command: () =>
+                signOut(this.auth).then(() => this.router.navigate([''])),
+            },
+          ],
+        } as MenuItem,
+      ];
+    }
+
+    // User is not logged in
+    return [
+      ...baseNavItems,
+      {
+        label: 'Login',
+        command: () => signInWithPopup(this.auth, new GoogleAuthProvider()),
+      },
+    ];
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { BrowserModule } from '@angular/platform-browser';
+import { MenubarModule } from 'primeng/menubar';
 import { StyleClassModule } from 'primeng/styleclass';
 import { environment } from 'src/environments/environment';
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 
 @NgModule({
@@ -16,6 +17,7 @@ import { PagesModule } from './pages/pages.module';
     provideAuth(() => getAuth()),
     AppRoutingModule,
     StyleClassModule,
+    MenubarModule,
     PagesModule,
   ],
   providers: [],
